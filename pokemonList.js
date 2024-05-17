5771b5751d8cc7776883d85685fb675e47d835f9
reagancart3r <reagancarter51@yahoo.com>
2024-05-17 16:08:32 -0500

    Pokemon data seperate file

---
+++ pokemonList.js
@@ -0,0 +1,101 @@
+// pokemonList.js
+const pokemonList = [
+    {
+        cardName: 'Raikou ex - Team Magma vs Team Aqua (MA)',
+        url: 'https://www.tcgplayer.com/product/88540/pokemon-team-magma-vs-team-aqua-raikou-ex?Condition=Lightly+Played&Language=English&page=1',
+        cardCondition: 'Lightly Played Holofoil',
+        lanuage: 'English',
+        desiredPrice: 80,
+        emailSent: false
+    },
+    {
+        cardName: 'Regirock ex - Hidden Legends (HL)',
+        url: 'https://www.tcgplayer.com/product/88674/pokemon-hidden-legends-regirock-ex?Condition=Lightly+Played&Language=English&page=1',
+        cardCondition: 'Lightly Played Holofoil',
+        lanuage: 'English',
+        desiredPrice: 30,
+        emailSent: false
+    },
+    {
+        cardName: 'Raikou (H26) - Skyridge (SK)',
+        url: 'https://www.tcgplayer.com/product/88531/pokemon-skyridge-raikou-h26?Condition=Lightly+Played&Language=English&page=1',
+        cardCondition: 'Lightly Played Holofoil',
+        lanuage: 'English',
+        desiredPrice: 200,
+        emailSent: false
+    },
+    {
+        cardName: 'Raikou (13) - Neo Revelation (N3)',
+        url: 'https://www.tcgplayer.com/product/88529/pokemon-neo-revelation-raikou-13?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 150,
+        emailSent: false
+    },
+    {
+        cardName: 'Suicune (14) - Neo Revelation (N3)',
+        url: 'https://www.tcgplayer.com/product/89597/pokemon-neo-revelation-suicune-14?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 150,
+        emailSent: false
+    },
+    {
+        cardName: 'Entei (6) - Neo Revelation (N3)',
+        url: 'https://www.tcgplayer.com/product/85266/pokemon-neo-revelation-entei-6?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 175,
+        emailSent: false
+    },
+    {
+        cardName: 'Celebi (3) - Neo Revelation (N3)',
+        url: 'https://www.tcgplayer.com/product/84141/pokemon-neo-revelation-celebi-3?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 100,
+        emailSent: false
+    },
+    {
+        cardName: 'Houndoom (4) - Neo Discovery (N2)',
+        url: 'https://www.tcgplayer.com/product/86197/pokemon-neo-discovery-houndoom-4?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 50,
+        emailSent: false
+    },
+    {
+        cardName: 'Houndour (5) - Neo Discovery (N2)',
+        url: 'https://www.tcgplayer.com/product/86216/pokemon-neo-discovery-houndour-5?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 100,
+        emailSent: false
+    },
+    {
+        cardName: 'Pichu - Neo Genesis (N1)',
+        url: 'https://www.tcgplayer.com/product/88011/pokemon-neo-genesis-pichu?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 60,
+        emailSent: false
+    },
+    {
+        cardName: 'Shining Mewtwo - Neo Destiny (N4)',
+        url: 'https://www.tcgplayer.com/product/89167/pokemon-neo-destiny-shining-mewtwo?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
+        cardCondition: 'Lightly Played 1st Edition Holofoil',
+        lanuage: 'English',
+        desiredPrice: 400,
+        emailSent: false
+    },
+    {
+        cardName: 'Mewtwo (Delta Species) - Delta Species (DS)',
+        url: 'https://www.tcgplayer.com/product/87425/pokemon-delta-species-mewtwo-delta-species?Condition=Lightly+Played&Language=English&page=1&Printing=Holofoil',
+        cardCondition: 'Lightly Played Holofoil',
+        lanuage: 'English',
+        desiredPrice: 40,
+        emailSent: false
+    }
+];
+
+module.exports = pokemonList;
