7faaa2ed42836c7ace782c689aa638f9e444fbaf
commitpending <reagancarter51@yahoo.com>
2024-05-17 10:44:20 -0500

    Added emailSent flag

--- scraper.js
+++ scraper.js
@@ -7,21 +7,24 @@ let pokemonList = [{
     url: 'https://www.tcgplayer.com/product/88540/pokemon-team-magma-vs-team-aqua-raikou-ex?Condition=Lightly+Played&Language=English&page=1',
     cardCondition: 'Lightly Played Holofoil',
     lanuage: 'English',
-    desiredPrice: 80
+    desiredPrice: 80,
+    emailSent: false
 },
 {
     cardName: 'Regirock ex - Hidden Legends (HL)',
     url: 'https://www.tcgplayer.com/product/88674/pokemon-hidden-legends-regirock-ex?Condition=Lightly+Played&Language=English&page=1',
     cardCondition: 'Lightly Played Holofoil',
     lanuage: 'English',
-    desiredPrice: 30
+    desiredPrice: 30,
+    emailSent: false
 },
 {
     cardName: 'Raikou (H26) - Skyridge (SK)',
     url: 'https://www.tcgplayer.com/product/88531/pokemon-skyridge-raikou-h26?Condition=Lightly+Played&Language=English&page=1',
     cardCondition: 'Lightly Played Holofoil',
     lanuage: 'English',
-    desiredPrice: 200
+    desiredPrice: 200,
+    emailSent: false
 },
 ,
 {
@@ -29,35 +32,40 @@ let pokemonList = [{
     url: 'https://www.tcgplayer.com/product/88529/pokemon-neo-revelation-raikou-13?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 150
+    desiredPrice: 150,
+    emailSent: false
 },
 {
     cardName: 'Suicune (14) - Neo Revelation (N3)',
     url: 'https://www.tcgplayer.com/product/89597/pokemon-neo-revelation-suicune-14?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 150
+    desiredPrice: 150,
+    emailSent: false
 },
 {
     cardName: 'Entei (6) - Neo Revelation (N3)',
     url: 'https://www.tcgplayer.com/product/85266/pokemon-neo-revelation-entei-6?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 175
+    desiredPrice: 175,
+    emailSent: false
 },
 {
     cardName: 'Celebi (3) - Neo Revelation (N3)',
     url: 'https://www.tcgplayer.com/product/84141/pokemon-neo-revelation-celebi-3?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 100
+    desiredPrice: 100,
+    emailSent: false
 },
 {
     cardName: 'Houndoom (4) - Neo Discovery (N2)',
     url: 'https://www.tcgplayer.com/product/86197/pokemon-neo-discovery-houndoom-4?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 50
+    desiredPrice: 50,
+    emailSent: false
 },
 ,
 {
@@ -65,28 +73,32 @@ let pokemonList = [{
     url: 'https://www.tcgplayer.com/product/86216/pokemon-neo-discovery-houndour-5?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 100
+    desiredPrice: 100,
+    emailSent: false
 },
 {
     cardName: 'Pichu - Neo Genesis (N1)',
     url: 'https://www.tcgplayer.com/product/88011/pokemon-neo-genesis-pichu?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 60
+    desiredPrice: 60,
+    emailSent: false
 },
 {
     cardName: 'Shining Mewtwo - Neo Destiny (N4)',
     url: 'https://www.tcgplayer.com/product/89167/pokemon-neo-destiny-shining-mewtwo?Condition=Lightly+Played&Language=English&page=1&Printing=1st+Edition+Holofoil',
     cardCondition: 'Lightly Played 1st Edition Holofoil',
     lanuage: 'English',
-    desiredPrice: 400
+    desiredPrice: 400,
+    emailSent: false
 },
 {
     cardName: 'Mewtwo (Delta Species) - Delta Species (DS)',
     url: 'https://www.tcgplayer.com/product/87425/pokemon-delta-species-mewtwo-delta-species?Condition=Lightly+Played&Language=English&page=1&Printing=Holofoil',
     cardCondition: 'Lightly Played Holofoil',
     lanuage: 'English',
-    desiredPrice: 40
+    desiredPrice: 40,
+    emailSent: false
 },
 ];
 
@@ -111,7 +123,7 @@ async function scrapeAndCheck(url, desiredPrice, cardCon,cardName, index) {
     for (let i = 0; i < numberPrices.length; i++) {
         if ( pokemonList[index].emailSent == false && numberPrices[i] <= desiredPrice && cardCondition[i] === cardCon) {
             console.log('a card was found under the desired price');
-            sendEmail('reagancarter1@outlook.com', `Price Alert - ${cardName} - $${desiredPrice}`, `The card ${cardName} is going for $${desiredPrice} on ${url}`);
+            sendEmail(process.env.SEND_EMAIL, `Price Alert - ${cardName} - $${desiredPrice}`, `The card ${cardName} is going for $${desiredPrice} on ${url}`);
             elementExists = true;
             pokemonList[index].emailSent = true;
 
@@ -130,13 +142,13 @@ function sendEmail(to, subject, text) {
     const transporter = nodemailer.createTransport({
         service: 'outlook',
         auth: {
-            user: 'cardsforadeal@outlook.com',
+            user: process.env.BOT_EMAIL,
             pass: process.env.EMAIL_PASSWORD
         }
     });
 
     const mailOptions = {
-        from: 'cardsforadeal@outlook.com',
+        from: process.env.BOT_EMAIL,
         to: to,
         subject: subject,
         text: text
