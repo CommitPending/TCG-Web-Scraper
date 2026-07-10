0fbf978437dc27151fae69479d5037d6acf4f422
CommitPending <reagancarter51@yahoo.com>
2026-07-09 21:24:34 -0500

    Use production environment secrets in workflow

--- data/cardList.js
+++ data/cardList.js
@@ -5,7 +5,7 @@ const cardList = [
         url: 'https://www.tcgplayer.com/product/88540/pokemon-team-magma-vs-team-aqua-raikou-ex?Condition=Lightly+Played&Language=English&page=1',
         cardCondition: 'Lightly Played Holofoil',
         lanuage: 'English',
-        desiredPrice: 800,
+        desiredPrice: 200,
         emailSent: false
     },
     {
